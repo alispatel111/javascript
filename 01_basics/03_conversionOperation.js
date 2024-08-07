245a10bc80e70b7b826048512e87ec99133f406d
Alis Patel <164079307+alispatel111@users.noreply.github.com>
2024-08-07 07:19:56 +0000

    next

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let value = 3
+
+let negValue =  -value
+
+// console.log(negValue);
+
+// operations //
+
+/* console.log(2+2);
+
+console.log(2-2);
+
+console.log(2*2);
+
+console.log(2**2);
+
+console.log(2%2);
+
+console.log(2/2);*/
+
+
+let str1 = "hello"
+
+let str2 = " alis"
+
+let str3 = str1 + str2
+
+/*console.log (str3);
+
+console.log("1" + 2)
+
+console.log(1 + "2")
+
+console.log("1" + 2 + 2)
+
+console.log(1 + 2 + "2")*/
+
+let gameCounter = 100
+
+++gameCounter//++//
+
+console.log(gameCounter);
+
+// prefix and postfix are imp //
---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,21 @@
+/*console.log("2" < 1)
+
+console.log("2" > 1)*/
+
+ /* console.log(null < 0)
+
+console.log(null == 0)
+
+console.log(null >= 0)
+
+console.log(undefined = 0)
+
+console.log(undefined < 0)
+
+console.log(undefined > 0) */
+
+// === it is a strict checking for both number comparision and datat type also check //
+
+// for ex //
+
+console.log("2" === 12);
